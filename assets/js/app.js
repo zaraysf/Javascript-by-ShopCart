ae388cda0bc68e3cfcdb351f50c29f37272c5b21
zaraysf <e402_404@yahoo.com>
2023-01-02 15:39:06 +1100

    problem solving Total Price

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,11 +41,10 @@ function createItemCart(product2) {
 
 function totalPrice(priceItem){
   const totalPrice = document.querySelector('.total-price');
-  // const btnP = document.querySelectorALL('.add-to-cart');
-  // const unitP = document.querySelector('.item-price');
-
-  return (totalPrice.innerText = Number(totalPrice.innerText) + Number(priceItem) );
- 
+  const priceNumber = Number(priceItem.replace(/,/g,"")) ;
+  
+    totalPrice.innerText = Number(totalPrice.innerText) + priceNumber;
+     
 }
 
 
@@ -61,6 +60,8 @@ btnSelect.forEach((element) => {
 
     const product = event.target.parentElement;
     const item = getProductInfo(product);
+
+     /////////////////// Total Price
     totalPrice(item.priceP);
 
 
@@ -95,98 +96,11 @@ btnSelect.forEach((element) => {
       ++qtyItem.innerText;// qtyItem.innerText = qtyItem.innerText + 1
     }
 
-    /////////////////// total price
+  
    
 
   });
 });
-
-
-
-
-
-/*function AddBtn() {
-  return `
-<div>
-  <button onclick='addItem(this)' class='add-btn'>Add <i class='fas fa-chevron-right'></i></button>
-</div>`;
-}
-
-function QtyBtn(qty = 1) {
-  if (qty == 0) return AddBtn();
-  return `
-<div>
-  <button class='btn-qty' onclick="qtyChange(this,'sub')"><i class='fas fa-chevron-left'></i></button>
-  <p class='qty'>${qty}</p>
-  <button class='btn-qty' onclick="qtyChange(this,'add')"><i class='fas fa-chevron-right'></i></button>
-</div>`;
-}
-
-function SwitchBtns(found) {
-  let element = found.getElementsByClassName("btn-add")[0];
-  element.classList.toggle("qty-change");
-  let hasClass = element.classList.contains("qty-change");
-  found.getElementsByClassName("btn-add")[0].innerHTML = hasClass
-    ? QtyBtn()
-    : AddBtn();
-}
-
-function ToggleBackBtns() {
-  let btns = document.getElementsByClassName("btn-add");
-  for (let btn of btns) {
-    if (btn.classList.contains("qty-change")) {
-      btn.classList.toggle("qty-change");
-    }
-    btn.innerHTML = AddBtn();
-  }
-}
-
-function qtyChange(event, handler) {
-  let btnClicked = event.parentElement.parentElement;
-  let isPresent = btnClicked.classList.contains("btn-add");
-  let itemName = isPresent
-    ? btnClicked.parentElement.parentElement.getElementsByClassName(
-        "product-name"
-      )[0].innerText
-    : btnClicked.parentElement.getElementsByClassName("name")[0].innerText;
-  let productNames = document.getElementsByClassName("product-name");
-  for (let name of productNames) {
-    if (itemName == name.innerText) {
-      let productBtn = name.parentElement.parentElement.getElementsByClassName(
-        "qty-change"
-      )[0];
-      cartDetails.forEach((item, i) => {
-        if (itemName == item.name) {
-          if (handler == "add" && item.qty < 10) {
-            item.qty += 1;
-            btnClicked.innerHTML = QtyBtn(item.qty);
-            productBtn.innerHTML = QtyBtn(item.qty);
-          } else if (handler == "sub") {
-            item.qty -= 1;
-            btnClicked.innerHTML = QtyBtn(item.qty);
-            productBtn.innerHTML = QtyBtn(item.qty);
-            if (item.qty < 1) {
-              cartDetails.splice(i, 1);
-              productBtn.innerHTML = AddBtn();
-              productBtn.classList.toggle("qty-change");
-            }
-          } else {
-            document.getElementsByClassName("purchase-cover")[0].style.display =
-              "block";
-            document.getElementsByClassName("stock-limit")[0].style.display =
-              "flex";
-            sideNav(0);
-          }
-        }
-      });
-    }
-  }
-  RenderCart();
-  CartIsEmpty();
-  CartItemsTotal();
-}*/
-
-
 
 
 
